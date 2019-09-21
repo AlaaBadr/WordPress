63f6997aa5d5863778c5fe0fb4612984450fe4b1
Alaa Badr <alaa.badr@seamlabs.com>
2019-09-21 18:03:06 +0200

    bug fix -> delete rows from wp tables on plugin deactivation now working

--- bedrock/web/app/plugins/events/uninstall.php
+++ bedrock/web/app/plugins/events/uninstall.php
@@ -12,6 +12,10 @@ function deactivatePlugin() {
     $table_name = $wpdb->prefix . 'events';
     $wpdb->query("DROP TABLE IF EXISTS $table_name");
 
+    $wpdb->delete('wp_posts', array( 'post_type' => 'event' ) );
+    $wpdb->query( 'DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT ID FROM wp_posts)' );
+    $wpdb->query( 'DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT ID FROM wp_posts)' );
+
     wp_delete_nav_menu( 'main-menu' );
 
 }
